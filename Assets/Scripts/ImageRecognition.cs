1d8c15f67208a83244c26a640d70e3570bff68b9
Alvaro Lacouture <lacouture100@gmail.com>
2020-03-29 20:27:15 -0400

    Added image tracking, missing camera focus

--- Assets/Scripts/ImageRecognition.cs
+++ Assets/Scripts/ImageRecognition.cs
@@ -1,18 +1,43 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
 
 public class ImageRecognition : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+
+
+    //Add our ReferenceImageManager
+    private ARTrackedImageManager _arTrackedImageManager;
+
+    private void Awake()
     {
-        
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+
+    }
+    private void OnEnable()
+    {
+        //tracks information about the images inside the manager, every time they are updated, added, deleted, etc.. 
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+
+    }
+    private void OnDisable()
+    {
+        //tracks information about the images inside the manager, every time they are updated, added, deleted, etc.. 
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
     {
-        
+        //print each of the ARTracked Images added since the last event
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
     }
 }
+ 
